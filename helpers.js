8a8131081534d20bcc16e88d9080594b18916779
Sierra05082004 <emisieher@gmail.com>
2026-04-15 19:49:20 -0400

    Database config and helper file initialization

---
+++ helpers.js
@@ -0,0 +1,18 @@
+//Helper functions for data validation
+//Could copy and add the necessary ones to client side validation
+
+//TODO validate new user fields
+
+
+
+
+//TODO validate posts fields
+
+
+
+
+//TODO validate join requests fields
+
+
+
+//TODO validate resport fields
